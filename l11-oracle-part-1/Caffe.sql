ebb217bfccb0e85b0c4c407837c4e47497acccd3
Academy contr <lukasowms@gmail.com>
2024-05-03 18:33:07 +0300

    Part 1 of Oracle

---
+++ l11-oracle-part-1/Caffe.sql
@@ -0,0 +1,42 @@
+-- DDL (Data Definition Language) CREATE, DROP, ALTER, TRUNCATE, RENAME
+-- DQL (Data Query Language) SELECT
+
+-- dual
+SELECT
+    1 + 1
+FROM
+    dual;
+
+SELECT
+    user
+FROM
+    dual;
+
+SELECT
+    sysdate
+FROM
+    dual;
+
+SELECT
+    *
+FROM
+    dual;
+
+SELECT
+    upper('some string')
+FROM
+    dual;
+
+
+-- DML (Data Manipulation Language) INSERT, UPDATE, DELETE
+-- DCL (Data Control Language) GRANT
+-- TCL
+
+
+
+
+
+
+
+
+
